7fc1212c3b01e1b33dd81033f665be6b344ccac2
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2018-11-23 17:28:01 +0530

    Component communication via props

--- src/index.js
+++ src/index.js
@@ -13,11 +13,30 @@ class Profile extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello {this.state.name}</h1>
-        <input onChange={(e) => this.setState({name: e.target.value})} value={this.state.name} />
+        <Greeting name={this.state.name} />
+        <ProfileEditor 
+          name={this.state.name}
+          onNameChanged={(newName) => this.setState({name: newName})} />
       </div>
     );
   }
+}
+
+function Greeting(props) {
+  return <h1>Hello {props.name}</h1>
+}
+
+function ProfileEditor(props) {
+  return (
+    <div>
+      <input onChange={(e) => props.onNameChanged(e.target.value)} value={props.name} />
+      <ClearProfile onNameChanged={props.onNameChanged} />
+    </div>
+  );
+}
+
+function ClearProfile(props) {
+  return <button onClick={() => props.onNameChanged("anjali")}>Reset</button>
 }
 
 ReactDOM.render(<Profile />, document.getElementById('root'));
