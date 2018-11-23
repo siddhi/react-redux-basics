984fffc0868a26b385798ea22244642cefe2e85d
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2018-11-23 17:13:59 +0530

    Hello world

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Hello() {
+  return (
+    <div>Hello, world!</div>
+  );
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
+
