ffaaa34ae62a8b48c2867b5c661b917cf7bc04b0
Siddharta Govindaraj <siddharta@silverstripesoftware.com>
2018-11-23 17:52:23 +0530

    Multiple reducers

--- src/store.js
+++ src/store.js
@@ -1,18 +1,36 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
-function reducer(state={name: "anjali"}, action) {
-  console.log("reducer", state, action);
+function profileReducer(state={name: "sid"}, action) {
   if (action.type === "NAME_CHANGE") {
     return {name: action.value};
   }
+  if (action.type === "NAME_CLEAR") {
+    return {name: ""};
+  }
   return state;
 }
+
+function notificationReducer(state={notifications: []}, action) {
+  if (action.type === "NEW_NOTIFICATION") {
+    return {notifications: state.notifications.concat(action.value)}
+  }
+  if (action.type === "NAME_CHANGE") {
+    return {notifications: state.notifications.concat("name changed to " + action.value)}  
+  }
+  return state;
+}
+
+let reducer = combineReducers({
+  profile: profileReducer,
+  notification: notificationReducer
+});
 
 let store = createStore(reducer);
 
 store.subscribe(() => console.log("store changed", store.getState()));
 
 store.dispatch({type: "NAME_CHANGE", value: "anand"});
+store.dispatch({type: "NEW_NOTIFICATION", value: "Message from sid"});
 store.dispatch({type: "NAME_CHANGE", value: "varsha"});
 
 export default store;
